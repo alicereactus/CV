886f935068a4fa57a465e4a5d84ac1dfe179ef57
alicereactus <agamia@mail.ru>
2021-03-25 14:52:31 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -36,15 +36,15 @@ function App() {
           </div>
           <div>
             <img src='https://www.flaticon.com/svg/static/icons/svg/2111/2111290.svg' />
-            <span><a href='https://github.com/alicereactus'>alicereactus</a></span>
+            <span><a href='https://github.com/alicereactus' target='_blank'>alicereactus</a></span>
           </div>
           <div>
             <img src='https://www.flaticon.com/svg/static/icons/svg/2111/2111368.svg' />
-            <span><a href='https://www.linkedin.com/in/alice-haidukievich-a88834192/'>alice-haidukievich-a88834192</a></span>
+            <span><a href='https://www.linkedin.com/in/alice-haidukievich-a88834192/' target='_blank'>alice-haidukievich-a88834192</a></span>
           </div>
           <div>
             <img src='https://www.flaticon.com/svg/static/icons/svg/2111/2111545.svg' />
-            <span>@wolfschpic</span>
+            <span><a href='https://telegram.me/wolfschpic' target='_blank'>@wolfschpic</a></span>
           </div>
           <div>
             <img src={LocationImage} />
@@ -124,15 +124,15 @@ function App() {
           <div className='myLanguagesContainer'>
             <div>
               <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Flag_of_Belarus_%281918%2C_1991%E2%80%931995%29.svg/1200px-Flag_of_Belarus_%281918%2C_1991%E2%80%931995%29.svg.png' />
-              <span>Belarusian (native speaker)</span>
+              <span>Belarusian (Native speaker)</span>
             </div>
             <div>
               <img src='https://img.icons8.com/color/2x/russian-federation.png' />
-              <span>Russian (native speaker)</span>
+              <span>Russian (Native speaker)</span>
             </div>
             <div>
               <img src='https://img.icons8.com/color/2x/great-britain.png' />
-              <span>English (Intermediate)</span>
+              <span>English (Upper Intermediate)</span>
             </div>
             <div>
               <img src='https://img.icons8.com/color/72/poland.png' />
@@ -149,10 +149,10 @@ function App() {
 
       <div className='sectionWithTextContainer'>
         <h1>ALESIA HAIDUKIEVIČ</h1>
-        <h2>Frontend Developer</h2>
+        <h2>Frontend Developer (ReactJS)</h2>
         <div>
           <h3>SUMMARY</h3>
-          <p>I'm responsible, motivated, easy-to-learn React Frontend Developer.
+          <p>I'm responsible, motivated, easy-to-learn Frontend Developer (RactJS).
           I like programming because it's interesting for me to create something, to make user interface alive and friendly.
           I like to study and make my code better, also I like to share experience and learn from other developers.</p>
           <p>I am 24 y.o. Now I live in Minsk (Belarus) but I consider options such as remote work and moving to another country.</p>
@@ -163,15 +163,22 @@ function App() {
             <div className='courseContainer'>
               <div className='placeContainer'>
                 <div className='place'>Freelance</div>
-                <div className='position'>React Frontend Developer</div>
+                <div className='position'>Frontend Developer (ReactJS)</div>
                 <div className='year'>June 2020 - up to now</div>
                 <div className='location'>Belarus, Minsk</div>
               </div>
-              <div className='description'> Studied and practiced: created reusable React class and functional components (with hooks); created new functionality using TS and JS; wrote unit-tests; created store with Redux; wrote requests to the server using axios; designed
-              project using Material UI. My projects: <a href='https://alicereactus.github.io/portfolio/' target='_blank'>click here</a>. 
+                <div className='description'>
+                  <div>Worked on an online store:</div>
+                  <div>-Created React functional components (with hooks): made a product card, сreated registration and login pages for users.</div>
+                  <div>-Organized work with the state using Redux.</div>
+                  <div>-Received information about products from the server, filled in product cards with information received from the server using axios, implemented requests for registration of users and for their login.</div>
+                  <div>-Created new functionality using TS and JS (adding and deleting products to the cart, editing product information).</div>
+                  <div>-Сovered the project with unit tests.</div>
+                  <div>-Designed project using Material UI.</div>
+                  <div><a href='https://alicereactus.github.io/portfolio/' target='_blank'>My portfolio</a>.</div>
               </div>
             </div>
-            <div className='courseContainer'>
+            {/* <div className='courseContainer'>
               <div className='placeContainer'>
                 <div className='place'>Accounting outsourcing private enterprise "IQ-consult"</div>
                 <div className='position'>Chief Accountant</div>
@@ -182,8 +189,8 @@ function App() {
               organizations in the field of legislation, prepared annual financial statements.
               Took part in the automation of company's accounting:
               wrote technical tasks for programmers.</div>
-            </div>
-            <div className='courseContainer'>
+            </div> */}
+            {/* <div className='courseContainer'>
               <div className='placeContainer'>
                 <div className='place'>School food factory</div>
                 <div className='position'>Accountant</div>
@@ -194,7 +201,7 @@ function App() {
               accounting reports.
               Took part in the automation of the company's accounting: advised programmers in the development of accounting
               software.</div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div>
@@ -202,7 +209,7 @@ function App() {
           <div className='educationContainer'>
             <div className='placeContainer'>
               <div className='place'>Belarusian State Economic University</div>
-              <div className='year'>August 2015 - June 2019</div>
+              <div className='year'>Aug 2015 - June 2019</div>
               <div className='location'>Belarus, Minsk</div>
             </div>
             <div className='description'>Bachelor's degree. Specialty: Accounting.</div>
@@ -211,21 +218,21 @@ function App() {
         <div>
           <h3>COURSES</h3>
           <div className='coursesContainer'>
-            <div className='courseContainer'>
+          <div className='courseContainer'>
               <div className='placeContainer'>
-                <div className='place'>Online courses in IT company Andersen</div>
-                <div className='year'>July 2020 - September 2020</div>
+                <div className='place'>Courses IT-incubator</div>
+                <div className='year'>May 2020 - Febr 2021</div>
                 <div className='location'>Belarus, Minsk</div>
               </div>
-              <div className='description'>Studied technologies: JS, HTML, CSS, Git, DevTools</div>
+              <div className='description'>Studied technologies: JS, HTML, CSS, ReactJS, TS, Redux, Redux-thunk, axios, REST API, Redux-form, formik, Material UI, Storybook</div>
             </div>
             <div className='courseContainer'>
               <div className='placeContainer'>
-                <div className='place'>Courses IT-incubator</div>
-                <div className='year'>May 2020 - January 2021</div>
+                <div className='place'>Online courses in IT company Andersen</div>
+                <div className='year'>July 2020 - Sept 2020</div>
                 <div className='location'>Belarus, Minsk</div>
               </div>
-              <div className='description'>Studied technologies: ReactJS, TS, JS, Redux, Redux-thunk, axios, REST API, HTML, CSS, Material UI, Storybook</div>
+              <div className='description'>Studied technologies: JS, HTML, CSS, Git, Chrome DevTools</div>
             </div>
           </div>
         </div>
